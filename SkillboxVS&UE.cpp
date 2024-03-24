13973a16fdee3fc8ba847ae249489384b4eee8c6
Kanstantsin Taukach <tovkachkot@gmail.com>
2024-03-24 18:22:03 +0300

    practical work 14.4

--- SkillboxVS&UE.cpp
+++ SkillboxVS&UE.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
 #include "Helpers.h"
 
 int main()
 {
-    int a = 3;
+   /* int a = 3;
     int b = 5;
     int res = SquareOfTheSum(a, b);
-    std::cout << "Square of the sum " << a << " and " << b << " is: " << res;
+    std::cout << "Square of the sum " << a << " and " << b << " is: " << res;*/
+
+    std::string str = "Hello, Artem!";
+    std::cout << str << std::endl;  
+
+    int length = str.length();
+
+    std::cout << "Length of string str: " << length << std::endl;
+    std::cout << "First char in string is: " << str[0] << std::endl;
+    std::cout << "Last char in string is: " << str[length - 1] << std::endl;
 
     return 0;
 }
