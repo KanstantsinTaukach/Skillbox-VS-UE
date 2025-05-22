914382a30a25a114f15f11e119477f6ddda7c10a
Kanstantsin Taukach <tovkachkot@gmail.com>
2025-05-22 12:04:52 +0300

    Virtual destructor

--- SkillboxVS&UE.cpp
+++ SkillboxVS&UE.cpp
@@ -4,32 +4,44 @@
 
 #pragma once
 
+class Parent
+{
+public:
+    virtual const char* getName() { return "Parent"; }
+
+    virtual ~Parent()
+    {
+        std::cout << "~Parent()" << std::endl;
+    }
+};
+
+class Child : public Parent
+{
+private:
+    int* m_array;
+
+public:
+    const char* getName() override { return "Child"; };
+
+    Child(int length)
+    {
+        m_array = new int[length];
+    }
+
+    ~Child() override
+    {
+        std::cout << "~Child()" << std::endl;
+        delete[] m_array;
+    }
+};
+
 int main()
 {
-    MyData<int> defaultData;
-    std::cout << "defaultData (default constructor)";
-    defaultData.printMyData();
+    Child child(5);
+    Parent& parent = child;
 
-    MyData<int> myDataWithData(2, 2);
+    std::cout << "parent.getName(): " << parent.getName() << std::endl;
+    std::cout << "parent.Parent::getName(): " << parent.Parent::getName() << std::endl;
 
-    myDataWithData.setData(0, 0, 1);
-    myDataWithData.setData(0, 1, 2);
-    myDataWithData.setData(1, 0, 3);
-    myDataWithData.setData(1, 1, 4);
-    
-    myDataWithData.setIntValue(8);
-    myDataWithData.setDoubleValue(7.77);
-    myDataWithData.setStringValue("new data");
-
-    std::cout << "myDataWithData (constructor with arguments)";
-    myDataWithData.printMyData();
-
-    defaultData = myDataWithData;
-    std::cout << "\ndefaultData (operator=)";
-    defaultData.printMyData();
-
-    MyData<int> copyConstructorData(defaultData);
-    std::cout << "copyConstructorData (copy constructor)";
-    defaultData.printMyData();
     return 0;
 }
