0a177aabdd88bb671c683fcbdc055603689f3abd
Kanstantsin Taukach <tovkachkot@gmail.com>
2025-06-12 17:48:22 +0300

    Implement Fraction class with exception handling

--- SkillboxVS&UE.cpp
+++ SkillboxVS&UE.cpp
@@ -1,36 +1,54 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <exception>
+#include <stdexcept>
 
 #pragma once
 
-class Parent
+class Fraction
 {
-public:
-	Parent() {}
-};
+private:
+	int numerator;
+	int denominator;
 
-class Child : public Parent
-{
 public:
-	Child() {}
+	Fraction(int _numerator, int _denominator) : numerator(_numerator), denominator(_denominator)
+	{
+		if (_denominator == 0)
+		{
+			throw::std::runtime_error("Denominator cannot be zero!\n");
+		}
+	}
+
+	void PrintFraction() const
+	{
+		std::cout << numerator << "/" << denominator << std::endl;
+	}
 };
 
 int main()
 {
+	int numerator, denomitator;
+
 	try
 	{
-		throw Child();
-	}
-	catch (Parent & parent)
-	{
-		std::cerr << "caught Parent";
-	}
-	catch (Child & child)
-	{
-		std::cerr << "caught Child";
-	}
+		std::cout << "Enter the first integer - numerator:\n";
+		if (!(std::cin >> numerator))
+		{
+			throw std::runtime_error("Invalid input. An integer is expected\n");
+		}
 
+		std::cout << "Enter the second integer - denominator:\n";
+		if (!(std::cin >> denomitator))
+		{
+			throw std::runtime_error("Invalid input. An integer is expected\n");
+		}
+
+		Fraction fr(numerator, denomitator);
+		fr.PrintFraction();
+	}
+	catch (std::exception& exception)
+	{
+		std::cerr << exception.what();
+	}
+	
 	return 0;
 }
